a3b011215f995fe90d8a843aa284aadaf9e36539
sabrine-elloumi <sabrineloumi6@gmail.com>
2025-10-20 21:28:04 +0200

    Integrate CORS and clean up server.js
    Added CORS support and improved code formatting.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 import express, { json } from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import authRoutes from './routes/auth.js';//authRoutes = LE "LIVREUR" qui apporte tes routes du fichier auth.js au serveur
-/* .env → fichier où tu mets tes secrets/config
-dotenv.config() → lit .env et met les valeurs dans Node
-process.env → objet qui contient toutes les variables d’environnement disponibles*/
-dotenv.config(); 
-const app= express();//fonction pré-définie qui crée une application Express
+import authRoutes from './routes/auth.js';
+import cors from 'cors'; 
 
-app.use(express.json());//comprendre les données envoyé avant de les utiliser
-app.use("/api/auth",authRoutes);
-app.listen(5000,()=>{//démarre le serveur sur un port spécifique.
+dotenv.config(); 
+const app = express();
+
+app.use(express.json());
+app.use(cors());  
+app.use("/api/auth", authRoutes);
+
+app.listen(5000, () => {
     connectDB();
     console.log("server started at http://localhost:5000");
 });
-
